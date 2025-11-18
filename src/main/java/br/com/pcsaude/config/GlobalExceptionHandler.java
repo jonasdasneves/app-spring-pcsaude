710a9792a173c698acab6f72c67084e74eafc849
jonasdasneves <jonas.joliveira2024@gmail.com>
2025-11-18 17:27:00 -0300

    (feat) Implementação de tratamento para novas exceções

--- src/main/java/br/com/pcsaude/config/GlobalExceptionHandler.java
+++ src/main/java/br/com/pcsaude/config/GlobalExceptionHandler.java
@@ -3,14 +3,25 @@ package br.com.pcsaude.config;
 import br.com.pcsaude.exceptions.ResourceNotFoundException;
 import br.com.pcsaude.exceptions.UniqueKeyDuplicadaException;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleNoResourceFoundException(NoResourceFoundException e) {}
+
     @ExceptionHandler(UniqueKeyDuplicadaException.class)
     public ResponseEntity<ErrorResponse> handleUniqueKeyDuplicadaException(ResourceNotFoundException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
@@ -22,11 +33,30 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        final Map<String, String> errors = new HashMap<>();
+        e.getBindingResult()
+                .getFieldErrors()
+                .stream().parallel()
+                .forEach( error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleException(Exception e) {
         e.printStackTrace();
         Map<String, String> errorResult = Map.of("error", "ops... ocorreu um erro inesperado");
         return ResponseEntity.internalServerError().body(errorResult);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String,String>> handleBadCredentialException(BadCredentialsException e) {
+        Map<String, String> errorResult = Map.of("error", "Usuário e/ou senha inválidos");
+        return ResponseEntity.badRequest().body(errorResult);
     }
 
     @Getter
